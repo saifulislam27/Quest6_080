7fab44c1e895740c58f3b4b7c3d1cb24618dc84b
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 15:13:53 +0700

    menambahkan di val options berisi string "Aplikasi Multimedia","Kecerdasan Buatan","Computer Vission"

--- app/src/main/java/com/example/project6/data/MataKuliah.kt
+++ app/src/main/java/com/example/project6/data/MataKuliah.kt
@@ -4,5 +4,8 @@ object MataKuliah {
     val options = listOf(
         "Pemrograman Aplikasi Mobile Lanjut",
         "Pengembangan Aplikasi Web Framework", 	"Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
     )
 }
