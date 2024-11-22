d33b0ff8ee596b81da7b77b61f8b596eccb36a00
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 16:13:03 +0700

    menambahkan variabel chosen dropdown

--- app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,3 +1,22 @@
 package com.example.project6.ui.view.viewmodel
 
-data class RencanaStudiViewModel()
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.project6.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+
+    }
+}
