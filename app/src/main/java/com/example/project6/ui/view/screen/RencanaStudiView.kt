933e2263fa1908454329cc4d76e639f473695b28
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 17:12:31 +0700

    menambahkan spacer, HorizontalDivider, Text, Row

--- app/src/main/java/com/example/project6/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/project6/ui/view/screen/RencanaStudiView.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -154,6 +155,22 @@ fun RencanaStudiView(
                                 Text(data)
                             }
                         }
+                    }
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(8.dp))
+
+                    Text(
+                        text = "Klausul Persetujuan Mahasiswa",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = { checked = it },
+                            enabled = chosenDropdown.isNotBlank() && pilihanKelas.isNotBlank()
+                        )
+
                     }
                 }
             }
