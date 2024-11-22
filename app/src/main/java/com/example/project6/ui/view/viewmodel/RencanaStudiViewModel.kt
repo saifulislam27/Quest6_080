461cbb95791c3ae479a067f530763772ebdb4ab8
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 16:36:30 +0700

    menambahkan image dari drawable image

--- app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.project6.ui.view.viewmodel
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.project6.R
 import com.example.project6.model.Mahasiswa
 import java.lang.reflect.Modifier
@@ -38,5 +44,20 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+
+        }
     }
 }
