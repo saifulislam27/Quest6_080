736582e86552e5557b83c0388c32936554a3447f
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 16:56:42 +0700

    menambahkan box

--- app/src/main/java/com/example/project6/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/project6/ui/view/screen/RencanaStudiView.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
@@ -88,7 +89,15 @@ fun RencanaStudiView(
                     tint = Color.White
                 )
             }
-        }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(topEnd = 15.dp, topStart = 15.dp)
+                    )
+            )
 
+        }
     }
 }
