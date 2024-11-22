1b328f6da986721840c5b5ff7da5183b2d6e2e9e
saifulislam27 <mariyatilambeja@gmail.com>
2024-11-22 16:21:27 +0700

    menambahkan column

--- app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/project6/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.project6.ui.view.viewmodel
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.colorResource
+import com.example.project6.R
 import com.example.project6.model.Mahasiswa
+import java.lang.reflect.Modifier
+
 
 @Composable
 fun RencanaStudyView(
@@ -27,4 +33,10 @@ fun RencanaStudyView(
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihankelas)
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+    }
 }
